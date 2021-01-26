64b1e79497e2f525b41c4ce0e9b057b9572fea95
Mariangel <mariangelacosta.88@gmail.com>
2021-01-26 11:13:32 -0500

    cards react

---
+++ Desktop/React-Academlo/01_Profile-Cards/src/components/Interests.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles.css'
+export const Interests = () => {
+	return (
+		<div className='container interests'>
+			<h2>Interes</h2>
+			
+				<li>
+					El desarrollo web
+				</li>
+				<li>
+					Conocer muchos lugares por todo el mundo
+				</li>
+				<li>
+					La robotica
+				</li>
+			
+		</div>
+	)
+}
